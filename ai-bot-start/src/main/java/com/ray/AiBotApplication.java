85f7d10eb16573c699c4f64d8880b953cfa0911c
rayluo <luozhiwei447@gmail.com>
2025-11-13 17:42:44 +0800

    Fix compilation issues and rename application class

---
+++ ai-bot-start/src/main/java/com/ray/AiBotApplication.java
@@ -0,0 +1,18 @@
+package com.ray;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * AI Bot应用启动类
+ * 
+ * @author ray
+ * @date 2024-11-13
+ */
+@SpringBootApplication
+public class AiBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AiBotApplication.class, args);
+    }
+}
